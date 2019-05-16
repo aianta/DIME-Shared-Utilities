d8610938b3a7fe9adca162cb06aa75879835fafc
Alexandru Ianta <aianta03@gmail.com>
2019-05-16 12:59:42 -0300

    exposing event service

---
+++ src/main/java/ca/oceansdata/dime/common/event/EventService.java
@@ -0,0 +1,25 @@
+package ca.oceansdata.dime.common.event;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.eventbus.EventBus;
+
+public class EventService {
+
+    private EventBus eb;
+
+    public EventService(EventBus eb){
+        this.eb = eb;
+    }
+
+    public void emit(EventType type, String target, JsonObject data){
+
+        JsonObject emission = new JsonObject()
+                .put("type", type.getText())
+                .put("target", target)
+                .put("data", data);
+
+        eb.send("dime.events", emission);
+
+    }
+
+}
