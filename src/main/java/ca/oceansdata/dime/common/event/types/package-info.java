7ef96575408986f9f6cf364f6fd158128de80d6b
Alex Ianta <aianta@dal.ca>
2019-06-15 14:29:17 -0300

    added package-info.java

---
+++ src/main/java/ca/oceansdata/dime/common/event/types/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "eventTypeImplementation", groupPackage = "ca.oceansdata.dime.common.event.types")
+package ca.oceansdata.dime.common.event.types;
+
+import io.vertx.codegen.annotations.ModuleGen;
