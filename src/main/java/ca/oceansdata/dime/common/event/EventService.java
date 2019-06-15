a41cc695dc3810f5685cae9307d8b0aac7a2fe91
Alex Ianta <aianta@dal.ca>
2019-06-15 16:52:42 -0300

    added event service parse method

--- src/main/java/ca/oceansdata/dime/common/event/EventService.java
+++ src/main/java/ca/oceansdata/dime/common/event/EventService.java
@@ -1,8 +1,10 @@
 package ca.oceansdata.dime.common.event;
 
 import ca.oceansdata.dime.common.DimeUtils;
+import ca.oceansdata.dime.common.event.types.*;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.core.eventbus.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +37,7 @@ public class EventService {
                 if(e.getType() == null){
                     throw new Exception("Event type cannot be null! Cannot emit event!");
                 }else{
-                    eb.publish("dime.events", e);
+                    eb.publish("dime.events", e.toJson());
                 }
             }
         }catch (Exception err){
@@ -44,6 +46,70 @@ public class EventService {
             err.printStackTrace();
         }
 
+
+    }
+
+    public static Event parse(Message msg){
+
+        JsonObject json = (JsonObject)msg.body();
+
+        try{
+            switch (json.getString("type")){
+                case "Attribute Mapping":
+                    return new AttributeMappingEvent(json);
+
+                case "Document Upload":
+                    return new DocumentUploadEvent(json);
+
+                case "Login":
+                    return new LoginEvent(json);
+
+                case "Logout":
+                    return new LogoutEvent(json);
+
+                case "Download Integrated File":
+                    return new DownloadIntegratedFileEvent(json);
+
+                case "Download Original File":
+                    return new DownloadOriginalFileEvent(json);
+
+                case "Community Task Created":
+                    return new CommunityTaskCreatedEvent(json);
+
+                case "Community Task Match":
+                    return new CommunityTaskMatchEvent(json);
+
+                case "Community Task Skip":
+                    return new CommunityTaskSkipEvent(json);
+
+                case "Community Task Feedback":
+                    return new CommunityTaskFeedbackEvent(json);
+
+                case "Community Match Result":
+                    return new CommunityMatchResultEvent(json);
+
+                case "Profile Field Update":
+                    return new ProfileFieldUpdateEvent(json);
+
+                case "Download DIME Tools for Windows":
+                    return new DownloadDimeToolsForWindowsEvent(json);
+
+                case "Edit Dataset Name":
+                    return new EditSchemaNameEvent(json);
+
+                case "Edit Dataset Description":
+                    return new EditSchemaDescriptionEvent(json);
+
+                default:
+                    log.error("No such event type!");
+            }
+        }catch (IllegalEventFormatException e){
+            log.error("Error parsing event object off the wire");
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
 
     }
 
