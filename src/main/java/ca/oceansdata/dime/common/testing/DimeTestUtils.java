288646b856c53ee3a96ca9ca4fc24ca951747cc5
Alex Ianta <aianta@dal.ca>
2019-07-11 13:30:31 -0300

    added testing utils!

---
+++ src/main/java/ca/oceansdata/dime/common/testing/DimeTestUtils.java
@@ -0,0 +1,30 @@
+package ca.oceansdata.dime.common.testing;
+
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.Vertx;
+
+public class DimeTestUtils {
+
+
+    /**Returns a config retriever for the specified json configuration file path. 
+     * @Author Alexandru Ianta
+     * @param vertx the vertx instance with which to create the config retriever
+     * @param path the path to the configuration file (in JSON format)
+     * @return
+     */
+    public static ConfigRetriever createConfigRetriever(Vertx vertx, String path){
+
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+                .setType("file")
+                .setConfig(new JsonObject().put("path", path));
+
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(fileStore);
+
+        return ConfigRetriever.create(vertx, options);
+
+    }
+
+}
